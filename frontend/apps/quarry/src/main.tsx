2644e8bd7854eff5181defabe5d0d435fa2dd29a
Quinntyne Brown <quinntynebrown@gmail.com>
2026-09-10 11:43:16 -0400

    feat: add responsive discovery presentation

--- frontend/apps/quarry/src/main.tsx
+++ frontend/apps/quarry/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { DiscoveryPage } from "./components/DiscoveryPage";
+import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode><DiscoveryPage /></StrictMode>);
